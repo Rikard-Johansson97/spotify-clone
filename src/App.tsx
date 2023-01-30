4b51cc53e346ff2db60dec11accb717b90e82df8
Rikard Johansson <Rikard.com@hotmail.com>
2023-01-30 15:48:01 +0100

    Added LocalStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import { getAccessToken } from "./utils/getAccessToken";
+import { getAccessTokenFromStorage } from "./utils/getAccessTokenFromStorage";
 
 function App() {
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    let accessToken = getAccessTokenFromStorage() || getAccessToken();
+    if (accessToken) {
+      sessionStorage.setItem("spotifyToken", accessToken);
+      setToken(accessToken);
+    }
+  }, []);
+
   return (
     <Box className='App'>
       <Routes>
